26cba45e95c1fd626757e803126dcc69bd4f36e5
Theweirdesthufflewpuff <75901727+Theweirdesthufflewpuff@users.noreply.github.com>
2021-03-29 18:45:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+canvas = document.getElementById('myCanvas');
+ ctx = canvas.getContext("2d"); car_width = 100;
+  car_height = 90;
+   car_image = "Car1.jpg";
+    car_x = 500; car_y = 100;
+    car_height = 90;
+    car_image = "car2.png"
+    car_x = 600; car_y = 100
+//defining a variable with a new image
+ background_imgTag.onload = uploadBackground;
+// setting a function, onloading this variable
+ background_imgTag.src = background_image;
+// load image
+ car_imgTag = new Image();
+//defining a variable with a new image
+ car_imgTag.onload = uploadrover;
+// setting a function, onloading this variable
+ car_imgTag.src = car_image;
+ // load image
